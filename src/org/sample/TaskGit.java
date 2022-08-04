e0cce69505c32ad228d346e31b173a1c522e1875
arushanvignesh <arushanvignesh@gmail.com>
2022-08-04 23:39:40 +0530

    my task

---
+++ src/org/sample/TaskGit.java
@@ -0,0 +1,43 @@
+package org.sample;
+
+public class TaskGit {
+	public static void main(String[] args) {
+		//dataType variableName = variable value;
+		byte age = 25;
+		System.out.println("your age =  "+age);
+		
+		short year = 1996;
+		System.out.println("year is "+year);
+
+	    int pin = 959595959;
+	    System.out.println("your pin is "+pin);
+	    System.out.println("your pin is "+pin+"100");//concatenation
+	    System.out.println("your pin is "+pin+100);//addition
+	    
+	    long phone = 9962431749l;
+	    System.out.println("mobile no is "+phone);
+	    
+	    float weight = 88.9f;
+	    System.out.println("your weight is "+weight);
+	      
+	    double height = 5.4;
+	    System.out.println("your height is  "+height+"ft");
+	    
+	    char gender = 'm';
+	    System.out.println("gender is "+gender);
+	    
+	    boolean b = true;
+	    System.out.println("sky is blue"+b);
+	    
+	    String email = "arushanvignesh@gmail.com";
+	    System.out.println("email is "+email);
+	    
+	    float x = 20/3f;
+	    System.out.println(x);
+	    
+	    double y = 20-3d;
+	    System.out.println(y);
+	}
+	}
+
+
